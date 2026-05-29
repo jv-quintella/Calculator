05eeb4dc65a679d11994798136e0a1e2f15e13dc
jv-quintella <quintella.jv@gmail.com>
2026-05-29 18:51:45 -0300

    feat: created fraction and percent functions

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 import math
+from fractions import Fraction
 
 def add(a, b):
     return a + b
@@ -24,11 +25,24 @@ def sqrt(a):
 def square(a):
     return a * a
 
-def percent(a):
-    pass
+def percent(a, b):
+    if b is not None:
+        return (a * b) / 100
+    else:
+        return a / 100
 
 def fraction(a, b):
-    pass
+    try:
+        if b is not None:
+
+            result = Fraction(a, b)
+        else:
+            result = Fraction(a)
+            
+        return str(result.limit_denominator())
+        
+    except ValueError:
+        return "Syntax error"
 
 def Menu():
     pass
