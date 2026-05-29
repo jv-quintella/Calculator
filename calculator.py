c95b5f1bb061f0cd9d34d0439509bb9b5ea8e64a
jv-quintella <quintella.jv@gmail.com>
2026-05-29 16:38:59 -0300

    feat: basic code structure

---
+++ calculator.py
@@ -0,0 +1,29 @@
+def sum():
+    pass
+
+def sub():
+    pass
+
+def prod():
+    pass
+
+def div():
+    pass
+
+def sqrt():
+    pass
+
+def square():
+    pass
+
+def percent():
+    pass
+
+def fraction():
+    pass
+
+def Menu():
+    pass
+
+def main():
+    pass
